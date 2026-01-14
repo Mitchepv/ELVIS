c0bdb28a13205f19513ccce2361c2e8c76c4157a
Mitchepv <143571339+Mitchepv@users.noreply.github.com>
2026-01-14 12:37:35 -0500

    Initial Commit

---
+++ ELVIS/ELVISApp.swift
@@ -0,0 +1,17 @@
+//
+//  ELVISApp.swift
+//  ELVIS
+//
+//  Created by Nia Mitchell on 1/14/26.
+//
+
+import SwiftUI
+
+@main
+struct ELVISApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
